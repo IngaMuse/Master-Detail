4db19abb23f6740661f8f02dd0ee18c628215da4
IngaMuse <ingamoshkareva@gmail.com>
2024-10-30 15:52:53 +0300

    feat: added item diolog

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -6,8 +6,17 @@ sap.ui.define(
     "sap/ui/model/FilterOperator",
     "sap/ui/model/Sorter",
     "sap/m/MessageBox",
+    "sap/ui/core/Fragment",
   ],
-  function (Controller, JSONModel, Filter, FilterOperator, Sorter, MessageBox) {
+  function (
+    Controller,
+    JSONModel,
+    Filter,
+    FilterOperator,
+    Sorter,
+    MessageBox,
+    Fragment
+  ) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
@@ -54,10 +63,89 @@ sap.ui.define(
       },
 
       onAdd: function () {
-        MessageBox.information("This functionality is not ready yet.", {
-          title: "Aw, Snap!",
+        this._loadCreateDialog();
+      },
+
+      _loadCreateDialog: async function () {
+        if (!this._oDialog) {
+          this._oDialog = await Fragment.load({
+            name: "sap.ui.demo.fiori2.view.fragment.CreateDialog",
+            controller: this,
+            id: this.getView().getId(),
+          }).then((oDialog) => {
+            this.getView().addDependent(oDialog);
+            oDialog.setDraggable(true);
+            return oDialog;
+          });
+        }
+        this._oDialog.open();
+      },
+
+      onDialogBeforeOpen(oEvent) {
+        const oDialog = oEvent.getSource();
+        const oParams = {
+          IntegrationID: "",
+        };
+        const oEntry = this.oModel.createEntry("/tItems", {
+          properties: oParams,
+        });
+        oDialog.setBindingContext(oEntry, "items");
+      },
+
+      onPressCancel() {
+        this.oModel.resetChanges();
+        this._oDialog.destroy();
+      },
+
+      onPressSave(oEvent) {
+        debugger;
+        const oDialog = this._oDialog,
+          oBindingContext = oDialog.getBindingContext("items");
+        const oData = oBindingContext.getObject();
+        oData.Quantity = Number(oData.Quantity);
+        oData.Price = Number(oData.Price);
+        this.oModel.submitChanges({
+          success: () => {
+            this._loadData();
+          },
+        });
+        this._oDialog.destroy();
+      },
+
+      _loadData: function () {
+        const oTable = this.byId("itemsTable");
+        oTable.getBinding("items").refresh();
+        oTable.getModel().read("/zjblessons_base_Items", {
+          success: function (oData) {
+            console.log(oData);
+          },
+          error: function (oError) {
+            console.log("Ошибка загрузки данных");
+          },
         });
       },
+
+      // onQuantityChange: function (oEvent) {
+      //   const oInput = oEvent.getSource();
+      //   const newValue = oInput.getValue();
+      //   console.log("Новое значение Quantity:", newValue);
+      //   const oDialog = this._oDialog;
+      //   const oBindingContext = oDialog.getBindingContext("items");
+
+      //   if (oBindingContext) {
+      //     const path = oBindingContext.getPath();
+      //     const oModel = oBindingContext.getModel();
+      //     oModel.setProperty(path + "/Quantity", newValue);
+      //   }
+
+      //   const oData = oBindingContext.getObject();
+      //   if (oData) {
+      //     oData.Quantity = Number(newValue); // Приведите к числу
+      //     console.log("Обновленный объект oData:", oData);
+      //   } else {
+      //     console.error("Контекст данных oData не найден");
+      //   }
+      // },
 
       onSort: function () {
         this._bDescendingSort = !this._bDescendingSort;
